12c679af96c4428e90a4d5ffc7ebead3beab14a6
Jim Hurne <jhurne@gradle.com>
2021-04-06 14:15:45 -0400

    Disable a Shellcheck false-positive related to printf

--- 01-validate-incremental-build.sh
+++ 01-validate-incremental-build.sh
@@ -249,6 +249,10 @@ info() {
 infof() {
   local format_string="$1"
   shift
+  # the format string is constructed from the caller's input. There is no
+  # good way to rewrite this that will not trigger SC2059, so outright
+  # disable it here.
+  # shellcheck disable=SC2059  
   printf "${YELLOW}${BOLD}${format_string}${RESTORE}\n" "$@"
 }
 
