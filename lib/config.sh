c1b7859f8d76dd9d3631cee99ba16bba58b9e6cd
Jim Hurne <jhurne@gradle.com>
2021-04-15 15:46:21 -0400

    Reword the prompts when running in interactive mode

--- lib/config.sh
+++ lib/config.sh
@@ -59,13 +59,13 @@ collect_project_details() {
      project_url=$_arg_git_url
   else
     echo
-    read -r -p "${USER_ACTION_COLOR}What is the project's GitHub URL?${RESTORE} " project_url
+    read -r -p "${USER_ACTION_COLOR}What is the project's Git URL?${RESTORE} " project_url
   fi
 
   if [ -n "${_arg_branch}" ]; then
      project_branch=$_arg_branch
   else
-     read -r -p "${USER_ACTION_COLOR}What branch should we checkout (press enter to use the project's default branch)?${RESTORE} " project_branch
+     read -r -p "${USER_ACTION_COLOR}What is the project's branch to check out? (the project's default branch)${RESTORE} " project_branch
   fi
 
   project_name=$(basename -s .git "${project_url}")
@@ -74,7 +74,7 @@ collect_project_details() {
 collect_gradle_task() { 
   if [ -z "$_arg_task" ]; then
     echo
-    read -r -p "${USER_ACTION_COLOR}What Gradle task do you want to run?  (assemble)${RESTORE} " task
+    read -r -p "${USER_ACTION_COLOR}What Gradle task do you want to run? (assemble)${RESTORE} " task
 
     if [[ "${task}" == "" ]]; then
       task=assemble
