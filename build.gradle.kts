439988df2aaa6dca65fc956b8587f0476d25524e
Jim Hurne <jhurne@gradle.com>
2021-04-22 16:04:51 -0400

    Update build to create zip files containing the scripts
    	- A separate zip file is created for Gradle and Maven.
    	- `*.m4` files are exlcuded because they are only needed when generating the
    	  parsing.sh library scripts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,32 @@
 
+plugins {
+  id("base")
+}
+
+tasks.register<Zip>("assembleGradleScripts") {
+  archiveAppendix.set("for-gradle")
+
+  from(layout.projectDirectory.dir("src/gradle"))
+  from(layout.projectDirectory.dir("src/")) {
+    include("lib/**")
+    exclude("lib/maven")
+    exclude("**/*.m4")
+  }
+}
+
+tasks.register<Zip>("assembleMavenScripts") {
+  archiveAppendix.set("for-maven")
+
+  from(layout.projectDirectory.dir("src/maven"))
+  from(layout.projectDirectory.dir("src/")) {
+    include("lib/**")
+    exclude("lib/gradle")
+    exclude("**/*.m4")
+  }
+}
+
+tasks.named("assemble") {
+  dependsOn("assembleGradleScripts")
+  dependsOn("assembleMavenScripts")
+}
 
