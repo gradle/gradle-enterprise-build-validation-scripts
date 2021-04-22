41c5ff4529ebd08d3d0330968dcd8521717c32e2
Jim Hurne <jhurne@gradle.com>
2021-04-22 14:51:24 -0400

    Replace the use of realpath with a pure-bash function
    Not every system has realpath installed. Doing this in straight Bash is
    surprisingly difficult.
    
    The function comes from [this Unix & Linux Stack Exchange Answer](https://unix.stackexchange.com/a/85068/88179)

--- lib/gradle.sh
+++ lib/gradle.sh
@@ -10,7 +10,7 @@ invoke_gradle() {
 
   # The gradle --init-script flag only accepts a relative directory path. ¯\_(ツ)_/¯
   local lib_dir_rel
-  lib_dir_rel=$(realpath --relative-to="$( pwd )" "${LIB_DIR}")
+  lib_dir_rel=$(relpath "$( pwd )" "${LIB_DIR}")
 
   local enable_ge_init_script
   enable_ge_init_script=()
@@ -38,3 +38,48 @@ make_local_cache_dir() {
   mkdir -p "${build_cache_dir}"
 }
 
+# From this Stack Exchange Community Wiki Answer: 
+# https://unix.stackexchange.com/a/85068/88179
+relpath() {
+    # both $1 and $2 are absolute paths beginning with /
+    # $1 must be a canonical path; that is none of its directory
+    # components may be ".", ".." or a symbolic link
+    #
+    # returns relative path to $2/$target from $1/$source
+    source=$1
+    target=$2
+
+    common_part=$source
+    result=
+
+    while [ "${target#"$common_part"}" = "$target" ]; do
+        # no match, means that candidate common part is not correct
+        # go up one level (reduce common part)
+        common_part=$(dirname "$common_part")
+        # and record that we went back, with correct / handling
+        if [ -z "$result" ]; then
+            result=..
+        else
+            result=../$result
+        fi
+    done
+
+    if [ "$common_part" = / ]; then
+        # special case for root (no common path)
+        result=$result/
+    fi
+
+    # since we now have identified the common part,
+    # compute the non-common part
+    forward_part=${target#"$common_part"}
+
+    # and now stick all parts together
+    if [ -n "$result" ] && [ -n "$forward_part" ]; then
+        result=$result$forward_part
+    elif [ -n "$forward_part" ]; then
+        # extra slash removal
+        result=${forward_part#?}
+    fi
+
+    printf '%s\n' "$result"
+}
