7d8c7a414f59a9ca25e4087064cb04af397a449c
Jim Hurne <jhurne@gradle.com>
2021-04-26 09:26:50 -0400

    Reformat build.gradle.kts to indent with four spaces

--- scripts/build.gradle.kts
+++ scripts/build.gradle.kts
@@ -1,8 +1,8 @@
 import de.undercouch.gradle.tasks.download.Download
 
 plugins {
-  id("base")
-  id("de.undercouch.download") version "4.1.1"
+    id("base")
+    id("de.undercouch.download") version "4.1.1"
 }
 
 base.archivesBaseName = "gradle-enterprise-experiments"
@@ -10,66 +10,66 @@ base.archivesBaseName = "gradle-enterprise-experiments"
 val argbashVersion by extra("2.10.0")
 
 tasks.register<Download>("downloadArgbash") {
-  src("https://github.com/matejak/argbash/archive/refs/tags/${argbashVersion}.zip")
-  dest(file("${buildDir}/argbash/argbash-${argbashVersion}.zip"))
+    src("https://github.com/matejak/argbash/archive/refs/tags/${argbashVersion}.zip")
+    dest(file("${buildDir}/argbash/argbash-${argbashVersion}.zip"))
 }
 
 tasks.register<Copy>("unpackArgbash") {
-  from(zipTree(tasks.getByName("downloadArgbash").outputs.files.singleFile))
-  into(layout.buildDirectory.dir("argbash"))
-  dependsOn("downloadArgbash")
+    from(zipTree(tasks.getByName("downloadArgbash").outputs.files.singleFile))
+    into(layout.buildDirectory.dir("argbash"))
+    dependsOn("downloadArgbash")
 }
 
 tasks.register("applyArgbash") {
-  dependsOn("unpackArgbash")
-  doLast {
-    val argbash = "${buildDir}/argbash/argbash-${argbashVersion}/bin/argbash"
-    val scripts = fileTree("src") {
-      include("**/parsing.sh")
-    }
+    dependsOn("unpackArgbash")
+    doLast {
+        val argbash = "${buildDir}/argbash/argbash-${argbashVersion}/bin/argbash"
+        val scripts = fileTree("src") {
+            include("**/parsing.sh")
+        }
 
-    scripts.forEach { file: File ->
-       logger.info("Applying argbash to $file")
-       exec {
-          commandLine(argbash, "-i", file)
-       }
+        scripts.forEach { file: File ->
+            logger.info("Applying argbash to $file")
+            exec {
+                commandLine(argbash, "-i", file)
+            }
+        }
     }
-  }
 }
 
 tasks.register<Zip>("assembleGradleScripts") {
-  archiveAppendix.set("for-gradle")
+    archiveAppendix.set("for-gradle")
 
-  from(layout.projectDirectory.dir("src/gradle"))
-  from(layout.projectDirectory.dir("src/")) {
-    include("lib/**")
-    exclude("lib/maven")
-    exclude("**/*.m4")
-  }
-  filter { line: String -> line.replace("/../lib","/lib") }
-  dependsOn("applyArgbash")
+    from(layout.projectDirectory.dir("src/gradle"))
+    from(layout.projectDirectory.dir("src/")) {
+        include("lib/**")
+        exclude("lib/maven")
+        exclude("**/*.m4")
+    }
+    filter { line: String -> line.replace("/../lib", "/lib") }
+    dependsOn("applyArgbash")
 }
 
 tasks.register<Zip>("assembleMavenScripts") {
-  archiveAppendix.set("for-maven")
+    archiveAppendix.set("for-maven")
 
-  from(layout.projectDirectory.dir("src/maven")) {
-    filter { line: String -> line.replace("/../lib","/lib") }
-  }
-  from(layout.projectDirectory.dir("src/")) {
-    include("lib/**")
-    exclude("lib/gradle")
-    exclude("**/*.m4")
-    filter { line: String -> line.replace("/../lib","/lib") }
-  }
-  from(rootProject.childProjects.get("capture-build-scans-maven-extension")!!.tasks.getByName("jar")) {
-    into("lib/maven/")
-  }
-  dependsOn("applyArgbash")
+    from(layout.projectDirectory.dir("src/maven")) {
+        filter { line: String -> line.replace("/../lib", "/lib") }
+    }
+    from(layout.projectDirectory.dir("src/")) {
+        include("lib/**")
+        exclude("lib/gradle")
+        exclude("**/*.m4")
+        filter { line: String -> line.replace("/../lib", "/lib") }
+    }
+    from(rootProject.childProjects.get("capture-build-scans-maven-extension")!!.tasks.getByName("jar")) {
+        into("lib/maven/")
+    }
+    dependsOn("applyArgbash")
 }
 
 tasks.named("assemble") {
-  dependsOn("assembleGradleScripts")
-  dependsOn("assembleMavenScripts")
+    dependsOn("assembleGradleScripts")
+    dependsOn("assembleMavenScripts")
 }
 
