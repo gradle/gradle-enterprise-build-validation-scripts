203bbbe4f116e2e76ac058f77c3de2ed5261f887
Jim Hurne <jhurne@gradle.com>
2021-05-03 09:55:20 -0400

    Change order of fields in scans.csv
    Also removed some unused variables.

--- scripts/lib/scan_info.sh
+++ scripts/lib/scan_info.sh
@@ -8,8 +8,8 @@ read_scan_info() {
   # but we control the CSV so we don't have to worry about various CSV edge cases
   while IFS=, read -r field_1 field_2 field_3; do
      base_url+=("$field_1")
-     scan_id+=("$field_2")
-     scan_url+=("$field_3")
+     scan_url+=("$field_2")
+     scan_id+=("$field_3")
   done < "${SCAN_FILE}"
 }
 
