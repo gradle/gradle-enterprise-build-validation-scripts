ed3def21808b002800a91ee00c27f095ba3243e0
Jim Hurne <jhurne@gradle.com>
2021-05-04 14:05:50 -0400

    Edit interactive text in Gradle experiment 03 to be similar to experiment 02

--- scripts/gradle/03-validate-build-caching-local-diff-place.sh
+++ scripts/gradle/03-validate-build-caching-local-diff-place.sh
@@ -83,13 +83,10 @@ wizard_execute() {
   collect_gradle_details
 
   print_bl
-  explain_local_cache_dir
-  make_local_cache_dir
-
-  print_bl
   explain_first_clone_project
   print_bl
   make_experiment_dir
+  make_local_cache_dir "${build_cache_dir}"
   git_clone_project "_1"
 
   print_bl
@@ -111,8 +108,9 @@ wizard_execute() {
   explain_warnings
 
   print_bl
-  explain_summary
-  explain_how_to_repeat_the_experiment
+  explain_measure_build_results
+  print_bl
+  explain_and_print_summary
   print_bl
 }
 
@@ -174,67 +172,42 @@ print_introduction() {
   IFS='' read -r -d '' text <<EOF
 $(print_introduction_title)
 
-This is the third of several experiments designed to help you
-optimize your team's builds. If you are running this experiment as part of a
-Gradle Enterprise Trial, then the experiments will also help you to build
-the data necessary to determine if Gradle Enerprise is useful to your
-organization.
+In this experiment, you will validate how well a given project leverages
+Gradle’s local build cache when built from different locations. A build is
+considered fully local-cache enabled if all tasks avoid performing any work
+because:
 
-This script (and the other experiment scripts) will run some of the
-experiment steps for you, but we'll walk you through each step so that you
-know exactly what we are doing, and why.
+  * The tasks' inputs have not changed since their last invocation and
+  * The tasks' outputs are present in the local cache
 
-In this experiment, we will be checking your build to see how well it takes
-advantage of the local build cache when the build is run from different
-locations.
+The goal of the experiment is to first identify those tasks that do not reuse
+outputs from the local cache, to then make an informed decision which of those
+tasks are worth improving to make your build faster, to then investigate why
+they did not reuse outputs, and to finally fix them once you understand the root
+cause.
 
-When the build cache is enabled, Gradle saves the output from tasks so that
-the same output can be reused if the task is executed again with the same
-inputs.  This is similar to incremental build, except that the cache is used
-across build runs. So even if you perform a clean, cached output will be
-used if the inputs to a task have not changed.
+The experiment can be run on any developer’s machine. It logically consists of
+the following steps:
 
-To test out the build cache, we will run two builds (with build caching
-enabled) against the same project, but in different locations. Both builds
-will invoke clean and run the same tasks. We will not make any changes
-between each build run (other than the location).
+  1. Run the Gradle build with the a typical task invocation including the 'clean' task
+  2. From a different check out of the same project, run the Gradle build with
+     the same task invocation including the 'clean' task
+  3. Determine which tasks are still executed in the second run and why
+  4. Assess which of the executed tasks are worth improving
 
-If the build is taking advantage of the local build cache, then very few (if
-any) tasks should actually execute on the second build (all of the task
-output should be used from the local cache).
+Step 1 and 2 should be executed with the local build cache enabled.
 
-The Gradle Solutions engineer will then work with you to figure out why some
-(if any) tasks ran on the second build, and how to optimize them to take
-advantage of the build cache.
+The script you have invoked automates the execution of step 1 and step 2 without
+modifying the project. Build scans support your investigation in step 3 and step
+4.
 
-${USER_ACTION_COLOR}Press <Enter> to get started.${RESTORE}
+After improving the build to make it leverage the local build cache, you can
+push your changes and run the experiment again. This creates a cycle of run →
+measure → improve → run.
+
+${USER_ACTION_COLOR}Press <Enter> to get started with the experiment.${RESTORE}
 EOF
 
-  print_wizard_text "${text}"
-  wait_for_enter
-}
-
-explain_local_cache_dir() {
-  local text
-  IFS='' read -r -d '' text <<EOF
-$(print_separator)
-${HEADER_COLOR}Configure local build cache${RESTORE}
-
-We are going to create a new empty local build cache dir (and configure
-Gradle to use it instead of the default local cache dir). This way, the
-first build won't find anything in the cache and all tasks will run.
-
-This is important because we want to make sure tasks that are cachable do in
-fact produce output that is stored in the cache.
-
-Specifically, we are going to create and use this directory for the local
-build cache (we'll delete it if it already exists from a previous run of the
-experiment):
-
-$(info "${build_cache_dir}")
-
-${USER_ACTION_COLOR}Press <Enter> to continue.${RESTORE}
-EOF
   print_wizard_text "${text}"
   wait_for_enter
 }
@@ -245,13 +218,15 @@ explain_first_clone_project(){
 $(print_separator)
 ${HEADER_COLOR}Check out project from Git for first build${RESTORE}
 
-For the first build, we are going create a fresh clone of the project.  That
-way, the experiment will be influenced by as few outside factors as
-possible.
+All configuration to run the experiment has been collected. For the first build,
+the Git repository that contains the project to validate will be checked out.
 
-We'll use a different clone of the project for the second build.
+A different check out of the project will be used for the second build.
 
-${USER_ACTION_COLOR}Press <Enter> to continue.${RESTORE}
+To keep the experiment isolated and repeatable, a clean local build cache
+directory was created.
+
+${USER_ACTION_COLOR}Press <Enter> to check out the project from Git.${RESTORE}
 EOF
   print_wizard_text "${text}"
   wait_for_enter
@@ -263,17 +238,11 @@ explain_first_build() {
 $(print_separator)
 ${HEADER_COLOR}Run first build${RESTORE}
 
-We are now ready to run the first build.
+Now that the project has been checked out, the first build can be run with the
+given Gradle tasks. The build will be invoked with the 'clean' task included and
+build caching disabled.
 
-We will execute 'clean ${tasks}'.
-
-We are invoking clean even though we just created a fresh clone because
-sometimes the clean task changes the order other tasks run in, which can
-impact how the build cache is utilized.
-
-We will also add a few build scan tags.
-
-${USER_ACTION_COLOR}Press <Enter> to run the first build.${RESTORE}
+${USER_ACTION_COLOR}Press <Enter> to run the first build of the experiment.${RESTORE}
 EOF
   print_wizard_text "${text}"
   wait_for_enter
@@ -285,12 +254,10 @@ explain_second_clone_project(){
 $(print_separator)
 ${HEADER_COLOR}Check out project from Git for second build${RESTORE}
 
-Before running the second build, we are going to clone the project into a
-different directory. If all goes well, the second build will use the build
-cache in exactly the same way as it does when we run multiple builds from
-the same location.
+Before running the second build, we are going to check out the project into a
+different directory.
 
-${USER_ACTION_COLOR}Press <Enter> to continue.${RESTORE}
+${USER_ACTION_COLOR}Press <Enter> to check out the project from Git to a second directory.${RESTORE}
 EOF
   print_wizard_text "${text}"
   wait_for_enter
@@ -303,14 +270,8 @@ explain_second_build() {
 $(print_separator)
 ${HEADER_COLOR}Run second build${RESTORE}
 
-Now we are going to run the build again without changing anything, other
-than the location (we're running this against the second clone of the
-project).
-
-In a fully optimized build, no tasks would run on this second build because
-we already built everything in the first build, and the task outputs should
-be in the local build cache. If some tasks do run, they will show up in the
-build scan for this second build.
+Now that the project has been checked out a second time, the second build can be
+run with the same Gradle tasks from the second check out.
 
 ${USER_ACTION_COLOR}Press <Enter> to run the second build.${RESTORE}
 EOF
@@ -318,69 +279,31 @@ EOF
   wait_for_enter
 }
 
-explain_summary() {
+explain_and_print_summary() {
   read_scan_info
   local text
   IFS='' read -r -d '' text <<EOF
-$(print_separator)
-${HEADER_COLOR}Measure build results${RESTORE}
+The 'Summary' section below captures the configuration of the experiment and the
+two build scans that were published as part of running the experiment.  The
+build scan of the second build is particularly interesting since this is where
+you can inspect what tasks were not leveraging Gradle’s local build cache.
 
-Now that both builds have completed, there is a lot of valuable data in
-Gradle Enterprise to look at. The data can help you find ineffiencies in
-your build.
+The 'Investigation Quick Links' section below allows quick navigation to the
+most relevant views in build scans to investigate what task outputs were fetched
+from the cache and what tasks executed in the second build with cache misses,
+which of those tasks had the biggest impact on build performance, and what
+caused the cache misses.
 
-After running the experiment, this script will generate a summary table of
-useful data and links to help you analyze the experiment results:
+The 'Command Line Invocation' section below demonstrates how you can rerun the
+experiment with the same configuration and in non-interactive mode.
 
-$(print_experiment_info)
+$(print_summary)
 
-"Experiment id" and "Experiment run id" are added as tags on the build
-scans.
+$(print_command_to_repeat_experiment)
 
-You can use the "Experiment id" to find all of the build scans for all runs
-of this experiment.
-
-Every time you run this script, we'll generate a unique "Experiment run id".
-You can use the run id to search for the build scans from a specific run of the
-experiment.
-
-$(print_build_scans)
-
-Above are links to the build scans from this experiment. A build scan provides
-a wealth of information and statistics about the build execution.
-
-$(print_quick_links)
-
-Use the above links help you get started in your analysis.
-
-The first link is to a comparison of the two build scans. Comparisons show you
-what was different between two different build executions.
-
-The "Task execution summary" shows overall statistics for the execution of
-the second build. You can use this link to get a quick overview of where
-there may be overall opportunities to optimize.
-
-The "Cache performance" link takes you to the build cache performance page
-of the 2nd build scan. This page contains various metrics related to the
-build cache (such as cache hits and misses).
-
-The "Executed tasks" link takes you to the timeline view of the second build
-scan and automatically shows only the tasks that were executed, sorted by
-execution time (with the longest-running tasks listed first). You can use
-this to quickly identify tasks that were executed again unnecessarily. You
-will want to optimize any such tasks that take a significant amount of time
-to complete.
-
-The "Executed cacheable tasks" link shows you which tasks ran again on the
-second build, but shouldn't have because they are actually cacheable. If any
-cacheable tasks ran, then one of their inputs changed (even though we didn't
-make any changes), or they may not be declaring their inputs correctly.
-
-The last link, "Non-cacheable tasks", shows you which tasks ran that are not
-cacheable. It is not always possible (or doesn't make sense) to cache the
-output from every task. For example, there is no way to cache the "output"
-of the clean task because the clean task deletes output rather than creating
-it.
+Once you have addressed the issues surfaced in build scans and pushed the
+changes to your Git repository, you can rerun the experiment and start over the
+cycle of run → measure → improve → run.
 EOF
   print_wizard_text "${text}"
 }
