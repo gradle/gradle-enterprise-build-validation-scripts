06e41400ed3aed66cf99fa272423888f570b8961
Jim Hurne <jhurne@gradle.com>
2021-05-06 14:16:42 -0400

    Use the LIB_DIR constant in libs.sh

--- scripts/lib/libs.sh
+++ scripts/lib/libs.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
 
 # shellcheck source=build-validation/scripts/lib/color.sh
-source "${SCRIPT_DIR}/../lib/color.sh" || { echo "Couldn't find '../lib/color.sh'"; exit 1; }
+source "${LIB_DIR}/color.sh" || { echo "Couldn't find '${LIB_DIR}/color.sh'"; exit 1; }
 
 # shellcheck source=build-validation/scripts/lib/info.sh
-source "${SCRIPT_DIR}/../lib/info.sh" || { echo "Couldn't find '../lib/info.sh'"; exit 1; }
+source "${LIB_DIR}/info.sh" || { echo "Couldn't find '${LIB_DIR}/info.sh'"; exit 1; }
 
 # shellcheck source=build-validation/scripts/lib/wizard.sh
-source "${SCRIPT_DIR}/../lib/wizard.sh" || { echo "Couldn't find '../lib/wizard.sh'"; exit 1; }
+source "${LIB_DIR}/wizard.sh" || { echo "Couldn't find '${LIB_DIR}/wizard.sh'"; exit 1; }
 
 # shellcheck source=build-validation/scripts/lib/config.sh
-source "${SCRIPT_DIR}/../lib/config.sh" || { echo "Couldn't find '../lib/config.sh'"; exit 1; }
+source "${LIB_DIR}/config.sh" || { echo "Couldn't find '${LIB_DIR}/config.sh'"; exit 1; }
 
 # shellcheck source=build-validation/scripts/lib/init.sh
-source "${SCRIPT_DIR}/../lib/init.sh" || { echo "Couldn't find '../lib/init.sh'"; exit 1; }
+source "${LIB_DIR}/init.sh" || { echo "Couldn't find '${LIB_DIR}/init.sh'"; exit 1; }
 
 # shellcheck source=build-validation/scripts/lib/git.sh
-source "${SCRIPT_DIR}/../lib/git.sh" || { echo "Couldn't find '../lib/git.sh'"; exit 1; }
+source "${LIB_DIR}/git.sh" || { echo "Couldn't find '${LIB_DIR}/git.sh'"; exit 1; }
 
 # shellcheck source=build-validation/scripts/lib/build_scan.sh
-source "${SCRIPT_DIR}/../lib/build_scan.sh" || { echo "Couldn't find '../lib/build_scan.sh'"; exit 1; }
+source "${LIB_DIR}/build_scan.sh" || { echo "Couldn't find '${LIB_DIR}/build_scan.sh'"; exit 1; }
 
 # shellcheck source=build-validation/scripts/lib/gradle.sh
-source "${SCRIPT_DIR}/../lib/gradle.sh" || { echo "Couldn't find '../lib/gradle.sh'"; exit 1; }
+source "${LIB_DIR}/gradle.sh" || { echo "Couldn't find '${LIB_DIR}/gradle.sh'"; exit 1; }
 
 # shellcheck source=build-validation/scripts/lib/maven.sh
-source "${SCRIPT_DIR}/../lib/maven.sh" || { echo "Couldn't find '../lib/maven.sh'"; exit 1; }
+source "${LIB_DIR}/maven.sh" || { echo "Couldn't find '${LIB_DIR}/maven.sh'"; exit 1; }
 
 # shellcheck source=build-validation/scripts/lib/help.sh
-source "${SCRIPT_DIR}/../lib/help.sh" || { echo "Couldn't find '../lib/help.sh'"; exit 1; }
+source "${LIB_DIR}/help.sh" || { echo "Couldn't find '${LIB_DIR}/help.sh'"; exit 1; }
 
 # shellcheck source=build-validation/scripts/lib/paths.sh
-source "${SCRIPT_DIR}/../lib/paths.sh" || { echo "Couldn't find '../lib/paths.sh'"; exit 1; }
+source "${LIB_DIR}/paths.sh" || { echo "Couldn't find '${LIB_DIR}/paths.sh'"; exit 1; }
