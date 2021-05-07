7d501b391a1adfd035ff10d73fd59dc9b49ff1d0
Jim Hurne <jhurne@gradle.com>
2021-05-07 09:41:25 -0400

    Rename receipt files to <build_scan_tag>-<timestamp>.receipt

--- scripts/lib/init.sh
+++ scripts/lib/init.sh
@@ -12,6 +12,6 @@ generate_run_id() {
 # Init common constants
 readonly RUN_ID=$(generate_run_id)
 readonly EXP_DIR="${SCRIPT_DIR}/.data/${SCRIPT_NAME%.*}/$(date +"%Y-%m-%dT%H_%M_%S")-${RUN_ID}"
-readonly RECEIPT_FILE="${EXP_DIR}/receipt-$(date +"%Y-%m-%dT%H_%M_%S")-${RUN_ID}.txt"
+readonly RECEIPT_FILE="${EXP_DIR}/${EXP_SCAN_TAG}-$(date +"%Y-%m-%dT%H_%M_%S").receipt"
 readonly BUILD_SCAN_FILE="${EXP_DIR}/build-scans.csv"
 readonly BUILD_CACHE_DIR="${EXP_DIR}/build-cache"
