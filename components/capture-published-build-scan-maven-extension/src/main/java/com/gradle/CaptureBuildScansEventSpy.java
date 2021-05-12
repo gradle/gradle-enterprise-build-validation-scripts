ab284740c72131592f4e1b337694d56cc4879c75
Jim Hurne <jhurne@gradle.com>
2021-05-12 16:21:59 -0400

    Updates the capture-build-scan Maven extension to capture the project name
    Getting the project by listening for the MavenExecutionResult event may not be
    the _best_ way to get the project, but it seems to work (at least based on some
    limited testing).
    
    The project cannot be outright injected and accessed directly because the
    current MavenProject is a "scoped" object (attempting to just have it injected
    fails with a Guice scope-related error).
    
    I'm not sure if the MavenExecutionResult event can occur in parallel to the
    BuildScanPublished event, so to be safe, access to the rootProjectName mutable
    property is synchronized.

--- components/capture-published-build-scan-maven-extension/src/main/java/com/gradle/CaptureBuildScansEventSpy.java
+++ components/capture-published-build-scan-maven-extension/src/main/java/com/gradle/CaptureBuildScansEventSpy.java
@@ -3,6 +3,8 @@ package com.gradle;
 import com.gradle.maven.extension.api.scan.BuildScanApi;
 import org.apache.maven.eventspy.AbstractEventSpy;
 import org.apache.maven.eventspy.EventSpy;
+import org.apache.maven.execution.MavenExecutionResult;
+import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.logging.Logger;
@@ -12,13 +14,13 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
 
 @Component( role = EventSpy.class, hint = "capture-build-scans" )
 public class CaptureBuildScansEventSpy  extends AbstractEventSpy {
 
     private final PlexusContainer container;
     private final Logger logger;
+    private String rootProjectName = "";
 
     @Inject
     public CaptureBuildScansEventSpy(PlexusContainer container, Logger logger) {
@@ -35,19 +37,40 @@ public class CaptureBuildScansEventSpy  extends AbstractEventSpy {
         if (buildScan == null) throw new RuntimeException("Unable to configure build scans: the Build Scan API cannot be found.");
 
         buildScan.buildScanPublished(scan -> {
-            logger.debug("Saving build scan data to build-scans.csv");
-            String port = scan.getBuildScanUri().getPort() != -1 ? ":" + scan.getBuildScanUri().getPort(): "";
-            String baseUrl = String.format("%s://%s%s", scan.getBuildScanUri().getScheme(), scan.getBuildScanUri().getHost(), port);
+            synchronized (rootProjectName) {
+                logger.debug("Saving build scan data to build-scans.csv");
+                String port = scan.getBuildScanUri().getPort() != -1 ? ":" + scan.getBuildScanUri().getPort() : "";
+                String baseUrl = String.format("%s://%s%s", scan.getBuildScanUri().getScheme(), scan.getBuildScanUri().getHost(), port);
 
-            try(FileWriter fw = new FileWriter("../build-scans.csv", true);
-                BufferedWriter bw = new BufferedWriter(fw);
-                PrintWriter out = new PrintWriter(bw))
-            {
-                out.println(String.format("%s,%s,%s", baseUrl, scan.getBuildScanUri(), scan.getBuildScanId()));
-            } catch (IOException e) {
-                logger.error("Unable to save scan data to build-scans.csv: " + e.getMessage(), e);
-                throw new RuntimeException("Unable to save scan data to build-scans.csv: " + e.getMessage(), e);
+                try (FileWriter fw = new FileWriter("../build-scans.csv", true);
+                     BufferedWriter bw = new BufferedWriter(fw);
+                     PrintWriter out = new PrintWriter(bw)) {
+                    out.println(String.format("%s,%s,%s,%s", rootProjectName, baseUrl, scan.getBuildScanUri(), scan.getBuildScanId()));
+                } catch (IOException e) {
+                    logger.error("Unable to save scan data to build-scans.csv: " + e.getMessage(), e);
+                    throw new RuntimeException("Unable to save scan data to build-scans.csv: " + e.getMessage(), e);
+                }
             }
         });
+    }
+
+    @Override
+    public void onEvent(Object event) throws Exception {
+        if(event instanceof MavenExecutionResult) {
+            MavenExecutionResult mavenExecutionResult = (MavenExecutionResult) event;
+
+            if (mavenExecutionResult.getProject() != null) {
+                MavenProject rootProject = walkToRootProject(mavenExecutionResult.getProject());
+
+                synchronized (rootProjectName) {
+                    rootProjectName = rootProject.getName();
+                }
+            }
+        }
+    }
+
+    private MavenProject walkToRootProject(MavenProject project) {
+        if(project.getParent() == null || project.isExecutionRoot()) return project;
+        return walkToRootProject(project.getParent());
     }
 }
