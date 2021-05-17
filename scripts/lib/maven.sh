f5a9156db0f011bb8045f63771568b5be56d0696
Jim Hurne <jhurne@gradle.com>
2021-05-17 09:51:08 -0400

    Use a bash array to build the args to pass to Maven
    This is a more robust approach because it handles shell qouting better. It also
    makes it easier to output the actual Maven command for debugging purposes. It
    is also more consistent with how we call out to other external programs.

--- scripts/lib/maven.sh
+++ scripts/lib/maven.sh
@@ -3,17 +3,22 @@
 readonly CAPTURE_SCANS_EXTENSION_JAR="${LIB_DIR}/maven/capture-published-build-scan-maven-extension-1.0.0-SNAPSHOT.jar"
 
 invoke_maven() {
+  local args
+  args=()
+
   pushd "${project_dir}" > /dev/null 2>&1 || die "ERROR: The subdirectory ${project_dir} (set with --project-dir) does not exist in ${project_name}." 3
 
-  ./mvnw \
-      -Dmaven.ext.class.path="${CAPTURE_SCANS_EXTENSION_JAR}" \
-      -Dscan.tag."${EXP_SCAN_TAG}" \
-      -Dscan.tag."${RUN_ID}" \
-      -Dorg.slf4j.simpleLogger.log.gradle.goal.cache=debug \
-      "$@" \
-      || die "ERROR: The experiment cannot continue because the build failed." $?
+  args+=(
+    -Dmaven.ext.class.path="${CAPTURE_SCANS_EXTENSION_JAR}"
+    -Dscan.tag."${EXP_SCAN_TAG}"
+    -Dscan.tag."${RUN_ID}"
+    -Dorg.slf4j.simpleLogger.log.gradle.goal.cache=debug
+    "$@"
+  )
+  debug ./mvnw "${args[@]}"
+  ./mvnw "${args[@]}" || die "ERROR: The experiment cannot continue because the build failed." $?
 
-  #shellcheck disable=SC2164  # This is extremely unlikely to fail
+  #shellcheck disable=SC2164  # This is extremely unlikely to fail, and even if it does, nothing terrible will happen.
   popd > /dev/null 2>&1
 }
 
