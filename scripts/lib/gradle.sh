0b4e07af303bf300dc8df5ce32c4dd7c11288b48
Jim Hurne <jhurne@gradle.com>
2021-05-17 09:36:18 -0400

    Use a bash array to build the arguments to pass to Gradle
    This is a more robust approach because it handles shell qouting better. It also
    makes it easier to output the actual Gradle command for debugging purposes.

--- scripts/lib/gradle.sh
+++ scripts/lib/gradle.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 invoke_gradle() {
-  local ge_server_arg
-  ge_server_arg=""
-  if [ -n "${ge_server}" ]; then
-    ge_server_arg="-Pcom.gradle.enterprise.init.script.server=${ge_server}"
-  fi
+  local args ge_server_arg
+  args=()
 
   pushd "${project_dir}" > /dev/null 2>&1 || die "ERROR: The subdirectory ${project_dir} (set with --project-dir) does not exist in ${project_name}." 3
 
@@ -13,33 +10,40 @@ invoke_gradle() {
   local lib_dir_rel
   lib_dir_rel="$(relative_lib_path)"
 
-  local enable_ge_init_script
   if [ "$enable_ge" == "on" ]; then
-    # FIXME This doesn't handle paths with spaces in it very well. Figure out how to do the shell qouting properly!
-    enable_ge_init_script="--init-script ${lib_dir_rel}/gradle/enable-gradle-enterprise.gradle"
+    args+=(--init-script "${lib_dir_rel}/gradle/enable-gradle-enterprise.gradle")
   fi
+
+  args+=(--init-script "${lib_dir_rel}/gradle/configure-gradle-enterprise.gradle")
+  args+=(--init-script "${lib_dir_rel}/gradle/capture-published-build-scan.gradle")
+
+  if [ -n "${ge_server}" ]; then
+    args+=("-Pcom.gradle.enterprise.init.script.server=${ge_server}")
+  fi
+
+  args+=(-Pcom.gradle.enterprise.init.script.experimentDir="${EXP_DIR}")
+  args+=(-Pcom.gradle.enterprise.init.script.expId=${EXP_SCAN_TAG})
+  args+=(-Pcom.gradle.enterprise.init.script.runId="${RUN_ID}")
+  args+=(-Dscan.capture-task-input-files)
+
+  # shellcheck disable=SC2206
+  args+=(${extra_args})
+  args+=("$@")
 
   rm -f "${EXP_DIR}/build-scan-publish-error.txt"
 
+  if [[ "$_arg_debug" == "on" ]]; then
+    echo ./gradlew "${args[@]}"
+  fi
+
   # shellcheck disable=SC2086
-  ./gradlew \
-      ${enable_ge_init_script} \
-      --init-script "${lib_dir_rel}/gradle/configure-gradle-enterprise.gradle" \
-      --init-script "${lib_dir_rel}/gradle/capture-published-build-scan.gradle" \
-      ${ge_server_arg} \
-      -Pcom.gradle.enterprise.init.script.experimentDir="${EXP_DIR}" \
-      -Pcom.gradle.enterprise.init.script.expId=${EXP_SCAN_TAG} \
-      -Pcom.gradle.enterprise.init.script.runId="${RUN_ID}" \
-      -Dscan.capture-task-input-files \
-      ${extra_args} \
-      "$@" \
-      || die "ERROR: The experiment cannot continue because the build failed." $?
+  ./gradlew "${args[@]}" || die "ERROR: The experiment cannot continue because the build failed." $?
 
   if [ -f "${EXP_DIR}/build-scan-publish-error.txt" ]; then
     die "ERROR: The experiment cannot continue because publishing the build scan failed." 2
   fi
 
-  #shellcheck disable=SC2164  # This is extremely unlikely to fail
+  #shellcheck disable=SC2164  # This is extremely unlikely to fail. and if it does, nothing really terrible will happen
   popd > /dev/null 2>&1
 }
 
