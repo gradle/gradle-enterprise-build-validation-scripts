fe2aab85b59689d7aadb09d18400bc611ae8a070
Etienne Studer <etienne@gradle.com>
2021-05-20 14:12:54 +0200

    Fine-tune error message

--- components/fetch-build-validation-data/src/main/java/com/gradle/enterprise/export_api/client/BuildScanNotFoundException.java
+++ components/fetch-build-validation-data/src/main/java/com/gradle/enterprise/export_api/client/BuildScanNotFoundException.java
@@ -4,8 +4,8 @@ import java.net.URL;
 
 public class BuildScanNotFoundException extends ExportApiClientException {
     public BuildScanNotFoundException(String buildScanId, URL gradleEnterpriseServer) {
-        super(String.format("Build scan %s was not found.%nVerify the build scan exists and that you have the" +
-                " 'Access build data via the Export API' permission.",
+        super(String.format("Build scan %s was not found.%nVerify the build scan exists and you have been granted the permission" +
+                " 'Access build data via the Export API'.",
             buildScanUrl(gradleEnterpriseServer, buildScanId)));
     }
 }
