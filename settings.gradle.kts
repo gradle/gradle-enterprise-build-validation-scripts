70f12c79c1190c30f99243f5fc556023f2b121b1
Nelson Osacky <nosacky@gradle.com>
2022-03-02 18:46:55 +0100

    Add build scan publication.
    Note: This does not add the access token to the `production-release.yml` workflow.
    Signed-off-by: Nelson Osacky <nosacky@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,27 @@
+import com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
+
+plugins {
+    id("com.gradle.enterprise") version "3.8.1"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.6.3"
+}
+
 rootProject.name = "build-validation"
+
+val isCI = System.getenv("GITHUB_ACTIONS") != null
+
+gradleEnterprise {
+    server = "https://ge.solutions-team.gradle.com"
+    buildScan {
+        capture {
+            isTaskInputFiles = true
+        }
+        isUploadInBackground = !isCI
+        publishAlways()
+        this as BuildScanExtensionWithHiddenFeatures
+        publishIfAuthenticated()
+    }
+}
+
 
 include("components/capture-build-scan-url-maven-extension")
 include("components/fetch-build-scan-data-cmdline-tool")
