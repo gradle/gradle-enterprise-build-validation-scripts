2747bd615b0bd946c06984cb1e6b98d7eafc18a4
Etienne Studer <etienne@gradle.com>
2022-03-25 17:47:36 +0100

    Revert version upgrade

--- components/fetch-build-scan-data-cmdline-tool/build.gradle.kts
+++ components/fetch-build-scan-data-cmdline-tool/build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:okhttp-sse")
 
-    implementation(platform("com.fasterxml.jackson:jackson-bom:2.13.2.1"))
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2.1")
+    implementation(platform("com.fasterxml.jackson:jackson-bom:2.13.2"))
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2")
 
     implementation("com.google.guava:guava:31.1-jre")
     implementation("info.picocli:picocli:4.6.3")
