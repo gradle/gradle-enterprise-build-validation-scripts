909efb87b4a2bcef9c1551da79f78f86f9d8bca1
Jim Hurne <jhurne@gradle.com>
2022-06-02 11:34:42 -0400

    Upgrade the swagger-codegen library

--- components/fetch-build-scan-data-cmdline-tool/build.gradle.kts
+++ components/fetch-build-scan-data-cmdline-tool/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    swaggerCodegen("org.openapitools:openapi-generator-cli:5.3.0")
+    swaggerCodegen("org.openapitools:openapi-generator-cli:5.4.0")
 
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.9.3"))
     implementation("com.squareup.okhttp3:okhttp")
