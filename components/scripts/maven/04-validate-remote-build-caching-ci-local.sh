dc4353cd914954f32b3000bcc043a7f3ee50d50f
Etienne Studer <etienne@gradle.com>
2022-06-07 10:00:02 +0200

    Update summary section

--- components/scripts/maven/04-validate-remote-build-caching-ci-local.sh
+++ components/scripts/maven/04-validate-remote-build-caching-ci-local.sh
@@ -508,20 +508,20 @@ explain_and_print_summary() {
   read_build_scan_metadata
   local text
   IFS='' read -r -d '' text <<EOF
-The ‘Summary’ section below captures the configuration of the experiment and the
-two build scans that were published as part of running the experiment.  The
-build scan of the second build is particularly interesting since this is where
-you can inspect what goals were not leveraging Gradle Enterprise's remote build cache.
+The ‘Summary‘ section below captures the configuration of the experiment and the
+two build scans that were published as part of running the experiment. The build
+scan of the second build is particularly interesting since this is where you can
+inspect what goals were not leveraging the remote build cache.
 
 The ‘Build Caching Leverage’ section below reveals the realized and potential
 savings from build caching. All cacheable goals' outputs need to be taken from
 the build cache in the second build for the build to be fully cacheable.
 
 The ‘Investigation Quick Links’ section below allows quick navigation to the
-most relevant views in build scans to investigate what goal outputs were fetched
-from the remote cache and what goals executed in the second build with cache
-misses, which of those goals had the biggest impact on build performance, and
-what caused the cache misses.
+most relevant views in build scans to investigate what goals were avoided due to
+remote build caching and what goals executed in the second build, which of those
+goals had the biggest impact on build performance, and what caused those goals
+to not be taken from the remote build cache.
 
 $(explain_command_to_repeat_experiment)
 
