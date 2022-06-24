584600ff145462664595a342b39e59b51345dc9a
Jim Hurne <jhurne@gradle.com>
2022-06-24 14:05:22 -0400

    Deduplicate mapping.example and network.settings
    Both iles are identical for Maven and Gradle. It is silly to maintain two
    copies of the same files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -110,6 +110,8 @@ val copyGradleScripts = tasks.register<Copy>("copyGradleScripts") {
     }
     from(layout.projectDirectory.dir("components/scripts")) {
         include("README.md")
+        include("mapping.example")
+        include("network.settings")
         include("lib/**")
         exclude("lib/cli-parsers")
         filter { line: String -> line.replace("<HEAD>", projectVersion) }
@@ -139,6 +141,8 @@ val copyMavenScripts = tasks.register<Copy>("copyMavenScripts") {
     }
     from(layout.projectDirectory.dir("components/scripts/")) {
         include("README.md")
+        include("mapping.example")
+        include("network.settings")
         include("lib/**")
         exclude("lib/cli-parsers")
         filter { line: String -> line.replace("<HEAD>", projectVersion) }
