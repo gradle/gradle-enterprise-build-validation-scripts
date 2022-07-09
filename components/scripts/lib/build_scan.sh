70f25802c1b099cbb3992f2b9380eb239d1aca2f
Etienne Studer <etienne@gradle.com>
2022-07-09 17:55:00 +0200

    Revert "Only add args for network settings if file exists"
    This reverts commit b267021ee6c2b00c6215c97d95184fbbc288f31d.

--- components/scripts/lib/build_scan.sh
+++ components/scripts/lib/build_scan.sh
@@ -166,9 +166,7 @@ fetch_and_read_build_scan_data() {
     args+=("--mapping-file" "${mapping_file}")
   fi
 
-  if [ -n "${SCRIPT_DIR}/network.settings" ]; then
-    args+=("--network-settings-file" "${SCRIPT_DIR}/network.settings")
-  fi
+  args+=("--network-settings-file" "${SCRIPT_DIR}/network.settings")
 
   if [[ "$1" == "build_cache_metrics_only" ]]; then
     build_cache_metrics_only="true"
