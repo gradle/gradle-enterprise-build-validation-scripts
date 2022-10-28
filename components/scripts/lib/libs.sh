a37484414567003e3305364890afeaff790c8cf4
Eric Haag <ehaag@gradle.com>
2022-10-28 08:12:03 -0500

    Alphabetize libs.sh

--- components/scripts/lib/libs.sh
+++ components/scripts/lib/libs.sh
@@ -12,38 +12,41 @@ failed_to_load_lib() {
   exit "${_UNEXPECTED_ERROR}"
 }
 
+# shellcheck source=lib/build_scan.sh
+source "${LIB_DIR}/build_scan.sh" || failed_to_load_lib build_scan.sh
+
 # shellcheck source=lib/color.sh
 source "${LIB_DIR}/color.sh" || failed_to_load_lib color.sh
-
-# shellcheck source=lib/exit-code.sh
-source "${LIB_DIR}/exit-code.sh" || failed_to_load_lib exit-code.sh
-
-# shellcheck source=lib/info.sh
-source "${LIB_DIR}/info.sh" || failed_to_load_lib info.sh
-
-# shellcheck source=lib/wizard.sh
-source "${LIB_DIR}/wizard.sh" || failed_to_load_lib wizard.sh
 
 # shellcheck source=lib/config.sh
 source "${LIB_DIR}/config.sh" || failed_to_load_lib config.sh
 
-# shellcheck source=lib/init.sh
-source "${LIB_DIR}/init.sh" || failed_to_load_lib init.sh
+# shellcheck source=lib/exit-code.sh
+source "${LIB_DIR}/exit-code.sh" || failed_to_load_lib exit-code.sh
 
 # shellcheck source=lib/git.sh
 source "${LIB_DIR}/git.sh" || failed_to_load_lib git.sh
 
-# shellcheck source=lib/build_scan.sh
-source "${LIB_DIR}/build_scan.sh" || failed_to_load_lib build_scan.sh
-
 # shellcheck source=lib/gradle.sh
 source "${LIB_DIR}/gradle.sh" || failed_to_load_lib gradle.sh
+
+# shellcheck source=lib/help.sh
+source "${LIB_DIR}/help.sh" || failed_to_load_lib help.sh
+
+# shellcheck source=lib/info.sh
+source "${LIB_DIR}/info.sh" || failed_to_load_lib info.sh
+
+# shellcheck source=lib/init.sh
+source "${LIB_DIR}/init.sh" || failed_to_load_lib init.sh
+
+# shellcheck source=lib/java.sh
+source "${LIB_DIR}/java.sh" || failed_to_load_lib java.sh
 
 # shellcheck source=lib/maven.sh
 source "${LIB_DIR}/maven.sh" || failed_to_load_lib maven.sh
 
-# shellcheck source=lib/help.sh
-source "${LIB_DIR}/help.sh" || failed_to_load_lib help.sh
+# shellcheck source=lib/parse.sh
+source "${LIB_DIR}/parse.sh" || failed_to_load_lib parse.sh
 
 # shellcheck source=lib/paths.sh
 source "${LIB_DIR}/paths.sh" || failed_to_load_lib paths.sh
@@ -51,8 +54,5 @@ source "${LIB_DIR}/paths.sh" || failed_to_load_lib paths.sh
 # shellcheck source=lib/project.sh
 source "${LIB_DIR}/project.sh" || failed_to_load_lib project.sh
 
-# shellcheck source=lib/java.sh
-source "${LIB_DIR}/java.sh" || failed_to_load_lib java.sh
-
-# shellcheck source=lib/parse.sh
-source "${LIB_DIR}/parse.sh" || failed_to_load_lib parse.sh
+# shellcheck source=lib/wizard.sh
+source "${LIB_DIR}/wizard.sh" || failed_to_load_lib wizard.sh
