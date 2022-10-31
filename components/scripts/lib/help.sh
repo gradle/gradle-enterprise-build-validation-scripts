d86ddfc5aa238fb15508fd34101385978d13ce7f
Jim Hurne <jhurne@gradle.com>
2022-10-31 09:56:02 -0400

    Fix help display for --disable-build-scan-publishing

--- components/scripts/lib/help.sh
+++ components/scripts/lib/help.sh
@@ -46,7 +46,7 @@ print_option_usage() {
        _print_option_usage "-e, --enable-gradle-enterprise" "Enables Gradle Enterprise on a project not already connected."
        ;;
     -x)
-       _print_option_usage "-x, --no-build-scan-publishing" "Disables the publication of build scans on the invoked builds."
+       _print_option_usage "-x, --disable-build-scan-publishing" "Disables the publication of build scans on the invoked builds."
        ;;
     -f)
        _print_option_usage "-f, --fail-if-not-fully-cacheable" "Terminates with exit code ${BUILD_NOT_FULLY_CACHEABLE} if the build is not fully cacheable."
