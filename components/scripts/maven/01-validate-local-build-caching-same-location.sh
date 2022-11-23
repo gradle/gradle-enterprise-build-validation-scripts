b1cb59ad4a729944fe7766dab188c8c6f0634657
Eric Haag <ehaag@gradle.com>
2022-11-23 13:26:27 -0600

    Update interactive output for exp1-maven when -x is used

--- components/scripts/maven/01-validate-local-build-caching-same-location.sh
+++ components/scripts/maven/01-validate-local-build-caching-same-location.sh
@@ -78,11 +78,15 @@ wizard_execute() {
   print_bl
   print_introduction
 
-  print_bl
-  explain_prerequisites_ccud_maven_extension "I."
-
-  print_bl
-  explain_prerequisites_api_access "II."
+  if [[ "${build_scan_publishing_mode}" == "on" ]]; then
+    print_bl
+    explain_prerequisites_ccud_maven_extension "I."
+    print_bl
+    explain_prerequisites_api_access "II."
+  else
+    print_bl
+    explain_prerequisites_ccud_maven_extension
+  fi
 
   print_bl
   explain_collect_git_details
@@ -132,7 +136,7 @@ execute_second_build() {
 }
 
 execute_build() {
-  info "./mvnw -Dscan -Dscan.tag.${EXP_SCAN_TAG} -Dscan.value.runId=${RUN_ID} clean ${tasks}$(print_extra_args)"
+  print_maven_command
 
   #shellcheck disable=SC2086  # we actually want ${tasks} to expand because it may have more than one maven goal
   invoke_maven \
@@ -141,6 +145,14 @@ execute_build() {
      -Dgradle.cache.local.directory="${BUILD_CACHE_DIR}" \
      clean ${tasks}
 }
+
+print_maven_command() {
+   if [[ "${build_scan_publishing_mode}" == "on" ]]; then
+     info "./mvnw -Dscan -Dscan.tag.${EXP_SCAN_TAG} -Dscan.value.runId=${RUN_ID} clean ${tasks}$(print_extra_args)"
+   else
+     info "./mvnw -Dscan.dump -Dscan.tag.${EXP_SCAN_TAG} -Dscan.value.runId=${RUN_ID} clean ${tasks}$(print_extra_args)"
+   fi
+ }
 
 fetch_build_cache_metrics() {
   if [ "$build_scan_publishing_mode" == "on" ]; then
@@ -256,19 +268,35 @@ EOF
 
 explain_measure_build_results() {
   local text
-  IFS='' read -r -d '' text <<EOF
+  if [[ "${build_scan_publishing_mode}" == "on" ]]; then
+    IFS='' read -r -d '' text <<EOF
 $(print_separator)
 ${HEADER_COLOR}Measure build results${RESTORE}
 
 Now that the second build has finished successfully, you are ready to measure in
-Gradle Enterprise how well your build leverages Gradle Enterprise's local build
-caching functionality for the invoked set of Maven goals.
+Gradle Enterprise how well your build leverages the local build cache for
+the invoked set of Maven goals.
 
 Some of the build scan data will be fetched from the build scans produced by the
 two builds to assist you in your investigation.
 
 ${USER_ACTION_COLOR}Press <Enter> to measure the build results.${RESTORE}
 EOF
+  else
+    IFS='' read -r -d '' text <<EOF
+$(print_separator)
+${HEADER_COLOR}Measure build results${RESTORE}
+
+Now that the second build has finished successfully, you are ready to measure in
+Gradle Enterprise how well your build leverages the local build cache for
+the invoked set of Maven goals.
+
+Some of the build scan data will be extracted from the locally stored, intermediate
+build data produced by the two builds to assist you in your investigation.
+
+${USER_ACTION_COLOR}Press <Enter> to measure the build results.${RESTORE}
+EOF
+  fi
   print_wizard_text "${text}"
   wait_for_enter
 }
@@ -276,15 +304,14 @@ EOF
 explain_and_print_summary() {
   read_build_scan_metadata
   local text
-  IFS='' read -r -d '' text <<EOF
+  if [[ "${build_scan_publishing_mode}" == "on" ]]; then
+    IFS='' read -r -d '' text <<EOF
 The ‘Summary’ section below captures the configuration of the experiment and the
 two build scans that were published as part of running the experiment. The build
 scan of the second build is particularly interesting since this is where you can
 inspect what goals were not leveraging the local build cache.
 
-The ‘Build Caching Leverage’ section below reveals the realized and potential
-savings from build caching. All cacheable goals' outputs need to be taken from
-the build cache in the second build for the build to be fully cacheable.
+$(explain_build_cache_leverage)
 
 The ‘Investigation Quick Links’ section below allows quick navigation to the
 most relevant views in build scans to investigate what goals were avoided due to
@@ -300,7 +327,34 @@ $(print_command_to_repeat_experiment)
 
 $(explain_when_to_rerun_experiment)
 EOF
+  else
+    IFS='' read -r -d '' text <<EOF
+The ‘Summary’ section below captures the configuration of the experiment. No
+build scans are available for inspection since publishing was disabled for the
+experiment.
+
+$(explain_build_cache_leverage)
+
+$(explain_command_to_repeat_experiment)
+
+$(print_summary)
+
+$(print_command_to_repeat_experiment)
+
+$(explain_when_to_rerun_experiment)
+EOF
+  fi
   print_wizard_text "${text}"
+}
+
+explain_build_cache_leverage() {
+  local text
+  IFS='' read -r -d '' text <<EOF
+The ‘Build Caching Leverage’ section below reveals the realized and potential
+savings from build caching. All cacheable goals' outputs need to be taken from
+the build cache in the second build for the build to be fully cacheable.
+EOF
+  echo -n "${text}"
 }
 
 process_arguments "$@"
