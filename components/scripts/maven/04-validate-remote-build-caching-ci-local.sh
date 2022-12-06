f22d104729f87f3f7db311a987767c9295bf4b86
Eric Haag <ehaag@gradle.com>
2022-12-06 14:17:41 -0600

    Fix command to repeat experiment early for exp4-maven

--- components/scripts/maven/04-validate-remote-build-caching-ci-local.sh
+++ components/scripts/maven/04-validate-remote-build-caching-ci-local.sh
@@ -451,8 +451,8 @@ EOF
   wait_for_enter
 }
 
-#Overrides config.sh#print_command_to_repeat_experiment
-print_command_to_repeat_experiment() {
+#Overrides config.sh#generate_command_to_repeat_experiment
+generate_command_to_repeat_experiment() {
   local cmd
   cmd=("./${SCRIPT_NAME}")
 
@@ -500,9 +500,7 @@ print_command_to_repeat_experiment() {
     cmd+=("-e")
   fi
 
-  info "Command Line Invocation"
-  info "-----------------------"
-  info "$(printf '%q ' "${cmd[@]}")"
+  printf '%q ' "${cmd[@]}"
 }
 
 explain_and_print_summary() {
