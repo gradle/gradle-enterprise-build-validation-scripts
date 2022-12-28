30bced9c9aa052e37bedfee9e0dc5ba49cbf5912
Eric Haag <ehaag@gradle.com>
2022-12-28 10:04:53 -0600

    Return all durations as milliseconds

--- components/fetch-build-scan-data-cmdline-tool/src/main/java/com/gradle/enterprise/cli/Fields.java
+++ components/fetch-build-scan-data-cmdline-tool/src/main/java/com/gradle/enterprise/cli/Fields.java
@@ -5,7 +5,6 @@ import com.gradle.enterprise.model.TaskExecutionSummary;
 
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.Locale;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -30,7 +29,7 @@ public enum Fields {
     EXECUTED_CACHEABLE_DURATION("Executed cacheable duration", d -> totalDuration(d, "executed_cacheable")),
     EXECUTED_NOT_CACHEABLE("Executed not cacheable", d -> totalTasks(d, "executed_not_cacheable")),
     EXECUTED_NOT_CACHEABLE_DURATION("Executed not cacheable duration", d -> totalDuration(d, "executed_not_cacheable")),
-    EFFECTIVE_TASK_EXECUTION_DURATION("Effective task execution duration", d -> String.valueOf(d.getEffectiveTaskExecutionDuration().toMillis())),
+    EFFECTIVE_TASK_EXECUTION_DURATION("Effective task execution duration", d -> formatDuration(d.getEffectiveTaskExecutionDuration())),
     ;
 
     public final String label;
@@ -76,19 +75,6 @@ public enum Fields {
     }
 
     private static String formatDuration(Duration duration) {
-        long hours = duration.toHours();
-        long minutes = duration.minusHours(hours).toMinutes();
-        double seconds = duration.minusHours(hours).minusMinutes(minutes).toMillis() / 1000d;
-
-        StringBuilder s = new StringBuilder();
-        if (hours != 0) {
-            s.append(hours + "h ");
-        }
-        if (minutes != 0) {
-            s.append(minutes + "m ");
-        }
-        s.append(String.format(Locale.ROOT, "%.3fs", seconds));
-
-        return s.toString().trim();
+        return String.valueOf(duration.toMillis());
     }
 }
