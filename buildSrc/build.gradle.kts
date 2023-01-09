808b3d049cafc237f8549c16c885b5555ed23627
Eric Haag <ehaag@gradle.com>
2023-01-09 11:45:08 -0600

    Add clarifying comment for why Azul was chosen

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 kotlin {
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(8))
+
+        // Azul has been chosen due to its wide range of supported platforms for
+        // Java 8, including arm64 for macOS.
+        // See: https://foojay.io/almanac/jdk-8
         vendor.set(JvmVendorSpec.AZUL)
     }
 }
