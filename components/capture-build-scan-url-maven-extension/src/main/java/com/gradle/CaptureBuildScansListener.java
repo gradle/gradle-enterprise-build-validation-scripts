446130c93a94a55cce4928d3eb7267d4d80ea281
Eric Haag <ehaag@gradle.com>
2023-03-09 14:16:12 -0500

    Rename variable names and system property

--- components/capture-build-scan-url-maven-extension/src/main/java/com/gradle/CaptureBuildScansListener.java
+++ components/capture-build-scan-url-maven-extension/src/main/java/com/gradle/CaptureBuildScansListener.java
@@ -47,8 +47,8 @@ public class CaptureBuildScansListener implements GradleEnterpriseListener {
             String runId = System.getProperty("com.gradle.enterprise.build-validation.runId");
             addCustomValueAndSearchLink(buildScan, "Experiment run id", runId);
 
-            String version = System.getProperty("com.gradle.enterprise.build-validation.version");
-            buildScan.value("Build validation scripts version", version);
+            String scriptsVersion = System.getProperty("com.gradle.enterprise.build-validation.scriptsVersion");
+            buildScan.value("Build validation scripts", scriptsVersion);
         });
     }
 
