6210bd01ca366e0667482e22932cd3968eafdd6c
Eric Haag <ehaag@gradle.com>
2023-03-22 18:24:57 -0500

    Add Javadoc to FailedRequestException

--- components/fetch-build-scan-data-cmdline-tool/src/main/java/com/gradle/enterprise/api/client/FailedRequestException.java
+++ components/fetch-build-scan-data-cmdline-tool/src/main/java/com/gradle/enterprise/api/client/FailedRequestException.java
@@ -4,22 +4,41 @@ import com.gradle.enterprise.cli.FetchToolException;
 
 import java.util.Optional;
 
+/**
+ * Exception thrown when a request to the Gradle Enterprise API fails.
+ */
 public class FailedRequestException extends FetchToolException {
 
     private final int httpStatusCode;
 
     private final String responseBody;
 
+    /**
+     * Constructs a new {@code FailedRequestException} from the specified {@link BuildScanUrl} and {@link ApiException}.
+     *
+     * @param buildScanUrl the url of the Build Scan that was being fetched when the exception occurred
+     * @param e the exception that was thrown while attempting to fetch the Build Scan
+     */
     public FailedRequestException(BuildScanUrl buildScanUrl, ApiException e) {
         super(buildMessage(buildScanUrl, e.getCode()));
         this.httpStatusCode = e.getCode();
         this.responseBody = e.getResponseBody();
     }
 
+    /**
+     * Returns the HTTP status code returned by the failed API request.
+     *
+     * @return the HTTP status code
+     */
     public int httpStatusCode() {
         return httpStatusCode;
     }
 
+    /**
+     * Returns an optional string containing the response body returned by the failed API request.
+     *
+     * @return an optional string containing the response body, or an empty optional if the response body was null
+     */
     public Optional<String> getResponseBody() {
         return Optional.ofNullable(responseBody);
     }
