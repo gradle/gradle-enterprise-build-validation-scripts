fe64a5d92249eda1ffa0ca3471ff7d2c764d7f7b
Etienne Studer <etienne@gradle.com>
2023-03-22 12:44:50 +0100

    Use a dedicated property to signal the omission of the server url validation

--- components/scripts/lib/gradle.sh
+++ components/scripts/lib/gradle.sh
@@ -23,11 +23,10 @@ invoke_gradle() {
   if [ -n "${ge_server}" ]; then
     args+=("-Dcom.gradle.enterprise.build-validation.gradle-enterprise.url=${ge_server}")
     args+=("-Dcom.gradle.enterprise.build-validation.gradle-enterprise.allow-untrusted-server=false")
-  elif [ "$build_scan_publishing_mode" == "off" ]; then
-    args+=("-Dcom.gradle.enterprise.build-validation.gradle-enterprise.url=https://0.0.0.0")
   fi
 
   if [[ "${build_scan_publishing_mode}" == "off" ]]; then
+    args+=("-Dcom.gradle.enterprise.build-validation.omitServerUrlValidation=true")
     args+=("-Dscan.dump")
   fi
 
