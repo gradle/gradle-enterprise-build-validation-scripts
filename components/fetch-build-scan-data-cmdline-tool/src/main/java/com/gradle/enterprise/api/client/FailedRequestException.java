9234216c6e9eed1653cb27238425cb1d13d15f7a
Eric Haag <ehaag@gradle.com>
2023-03-22 16:59:37 -0500

    Make exception message building consistent

--- components/fetch-build-scan-data-cmdline-tool/src/main/java/com/gradle/enterprise/api/client/FailedRequestException.java
+++ components/fetch-build-scan-data-cmdline-tool/src/main/java/com/gradle/enterprise/api/client/FailedRequestException.java
@@ -10,7 +10,7 @@ public class FailedRequestException extends ApiClientException {
     private final String responseBody;
 
     public static FailedRequestException fromApiException(URL baseUrl, String buildScanId, ApiException e) {
-        final String message = buildMessage(baseUrl, buildScanId, e);
+        final String message = buildMessage(baseUrl, buildScanId, e.getCode());
         return new FailedRequestException(message, e.getCode(), e.getResponseBody(), e);
     }
 
@@ -28,21 +28,18 @@ public class FailedRequestException extends ApiClientException {
         return Optional.ofNullable(responseBody);
     }
 
-    private static String buildMessage(URL baseUrl, String buildScanId, ApiException e) {
-        switch (e.getCode()) {
+    private static String buildMessage(URL baseUrl, String buildScanId, int code) {
+        final URL buildScanUrl = buildScanUrl(baseUrl, buildScanId);
+        switch (code) {
             case StatusCodes.NOT_FOUND:
                 return String.format("Build scan %s was not found.%nVerify the build scan exists and you have been" +
-                        "granted the permission 'Access build data via the Export API'.",
-                        buildScanUrl(baseUrl, buildScanId));
+                        "granted the permission 'Access build data via the Export API'.", buildScanUrl);
             case StatusCodes.UNAUTHORIZED:
-                return String.format("Failed to authenticate while attempting to fetch build scan %s.",
-                        buildScanUrl(baseUrl, buildScanId));
+                return String.format("Failed to authenticate while attempting to fetch build scan %s.", buildScanUrl);
             case 0:
-                return String.format("Unable to connect to %s in order to fetch build scan %s: %s",
-                        baseUrl, buildScanId, e.getMessage());
+                return String.format("Unable to connect to server in order to fetch build scan %s.", buildScanUrl);
             default:
-                return String.format("Encountered an unexpected response while fetching build scan %s.",
-                        buildScanUrl(baseUrl, buildScanId));
+                return String.format("Encountered an unexpected response while fetching build scan %s.", buildScanUrl);
         }
     }
 }
