ab2c565609035de1300022256ee051f2616a4158
Eric Haag <ehaag@gradle.com>
2023-03-22 19:04:37 -0500

    Clarify Javadoc

--- components/fetch-build-scan-data-cmdline-tool/src/main/java/com/gradle/enterprise/api/client/ApiClientException.java
+++ components/fetch-build-scan-data-cmdline-tool/src/main/java/com/gradle/enterprise/api/client/ApiClientException.java
@@ -1,7 +1,8 @@
 package com.gradle.enterprise.api.client;
 
 /**
- * Exception thrown when an error occurs while fetching a Build Scan.
+ * Exception thrown by the {@link GradleEnterpriseApiClient} when an error
+ * occurs while fetching a Build Scan from the Gradle Enterprise API.
  */
 public abstract class ApiClientException extends RuntimeException {
 
