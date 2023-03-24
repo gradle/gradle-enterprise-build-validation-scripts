cb108b8ea24925591dc75f8879871acd9b9f6ccf
Eric Haag <ehaag@gradle.com>
2023-03-24 13:59:04 -0500

    Replaces usages of capture-build-scan-url to configure-gradle-enterprise

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ val mavenComponents by configurations.creating
 dependencies {
     argbash("argbash:argbash:2.10.0@zip")
     commonComponents(project(path = ":fetch-build-scan-data-cmdline-tool", configuration = "shadow"))
-    mavenComponents(project(":capture-build-scan-url-maven-extension"))
+    mavenComponents(project(":configure-gradle-enterprise-maven-extension"))
     mavenComponents("com.gradle:gradle-enterprise-maven-extension:1.16.5")
     mavenComponents("com.gradle:common-custom-user-data-maven-extension:1.11.1")
 }
