5ddd0ae27fd2fa5bb1b2d81d510a926c810c6d38
Eric Haag <ehaag@gradle.com>
2023-03-27 12:45:52 -0500

    Add enum class to represent fields for build time metrics

---
+++ components/fetch-build-scan-data-cmdline-tool/src/main/java/com/gradle/enterprise/cli/BuildTimeMetricsFields.java
@@ -0,0 +1,23 @@
+package com.gradle.enterprise.cli;
+
+import com.gradle.enterprise.model.BuildValidationData;
+
+import java.util.function.Function;
+
+public enum BuildTimeMetricsFields {
+    // The order the enums are defined controls the order the fields are printed in the CSV
+    INITIAL_BUILD_TIME("Initial Build Time", d -> ""),
+    INSTANT_SAVINGS("Instant Savings", d -> ""),
+    INSTANT_SAVINGS_BUILD_TIME("Instant Savings Build Time", d -> ""),
+    PENDING_SAVINGS("Pending Savings", d -> ""),
+    PENDING_SAVINGS_BUILD_TIME("Pending Savings Build Time", d -> ""),
+    ;
+
+    public final String label;
+    public final Function<BuildValidationData, String> value;
+
+    BuildTimeMetricsFields(String label, Function<BuildValidationData, String> value) {
+        this.label = label;
+        this.value = value;
+    }
+}
