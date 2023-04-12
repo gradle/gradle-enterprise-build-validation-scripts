f0b8b5daba37c1f44e52146775042281d878b7bd
Eric Haag <ehaag@gradle.com>
2023-04-11 22:41:12 -0500

    Logging level when fetching Build Scan data from Gradle Enterprise is incorrect (#397)
    * Flip condition for when brief logging is used
    
    * Update release.md
    
    * Use verbose logging when fetching single Build Scan
    
    * Wrap verbose_logging in quotes

--- components/scripts/lib/build-scan-online.sh
+++ components/scripts/lib/build-scan-online.sh
@@ -48,7 +48,7 @@ fetch_single_build_scan() {
   local build_scan_url="$1"
 
   local build_scan_data
-  build_scan_data="$(fetch_build_scan_data brief_logging "${build_scan_url}")"
+  build_scan_data="$(fetch_build_scan_data 'verbose_logging' "${build_scan_url}")"
 
   parse_single_build_scan "${build_scan_data}"
 }
@@ -59,8 +59,9 @@ fetch_build_scans_and_build_time_metrics() {
   local build_scan_urls=("$@")
 
   local brief_logging
-  if [[ "$build_cache_metrics_only" != 'build_cache_metrics_only' ]]; then
+  if [[ "${build_cache_metrics_only}" == 'build_cache_metrics_only' ]]; then
     brief_logging="brief_logging"
+  else
     info "Fetching build scan data"
   fi
 
