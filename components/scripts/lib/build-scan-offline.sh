fb5d5bc2addbbf048bd7c29bb31157b8782048d2
Eric Haag <ehaag@gradle.com>
2023-08-29 22:28:46 -0500

    Add error message for invalid JVM version
    Adds specific error handling for an invalid JVM version. Other non-zero
    exit codes are treated as an unexpected error.

--- components/scripts/lib/build-scan-offline.sh
+++ components/scripts/lib/build-scan-offline.sh
@@ -2,6 +2,10 @@
 
 readonly READ_BUILD_SCAN_DATA_JAR="${LIB_DIR}/build-scan-clients/build-scan-summary-${SUMMARY_VERSION}.jar"
 
+# Build scan summary exit codes
+readonly SUCCESS=0
+readonly JVM_VERSION_NOT_SUPPORTED=3
+
 build_scan_dumps=()
 
 find_and_read_build_scan_dumps() {
@@ -28,16 +32,19 @@ find_build_scan_dump() {
 }
 
 read_build_scan_dumps() {
-  local build_scan_data args
-  args=()
+  local args build_scan_data build_scan_summary_exit_code
 
-  args+=(
-      "0,file://${build_scan_dumps[0]}"
-      "1,file://${build_scan_dumps[1]}"
+  args=(
+    "0,file://${build_scan_dumps[0]}"
+    "1,file://${build_scan_dumps[1]}"
   )
 
   echo "Extracting Build Scan data for all builds"
-  if ! build_scan_data="$(JAVA_HOME="${CLIENT_JAVA_HOME:-$JAVA_HOME}" invoke_java "$READ_BUILD_SCAN_DATA_JAR" "${args[@]}")"; then
+  build_scan_data="$(JAVA_HOME="${CLIENT_JAVA_HOME:-$JAVA_HOME}" invoke_java "$READ_BUILD_SCAN_DATA_JAR" "${args[@]}")"
+  build_scan_summary_exit_code="$?"
+  if [[ $build_scan_summary_exit_code -eq $JVM_VERSION_NOT_SUPPORTED ]]; then
+    die "ERROR: Java 17+ is required when using --disable-build-scan-publishing. Rerun the script with Java 17+ or set the environment variable CLIENT_JAVA_HOME to a Java 17+ installation." "$UNEXPECTED_ERROR"
+  elif [[ $build_scan_summary_exit_code -ne $SUCCESS ]]; then
     exit "$UNEXPECTED_ERROR"
   fi
   echo "Finished extracting Build Scan data for all builds"
